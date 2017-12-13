dc06f830a5a97ba0c64178d83cdf264ab8db9734
hanlifeng <hanlifeng@hanlifeng.Control.TMSR>
2017-12-13 14:31:11 +0800

    express generate

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+
+// app.set('ejs', path.join(__dirname, 'ejs'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
