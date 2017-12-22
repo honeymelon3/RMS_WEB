2651c71d35c40b22a4816475726c23f9c886b4bc
Johnson Han <hanlifeng@sinap.ac.cn>
2017-12-22 13:07:08 +0800

    更新路由system_structure

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/system_structure');
 
 var app = express();
 
