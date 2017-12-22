412420a9719a35afadaf3c937282d593f918b63b
Johnson Han <hanlifeng@sinap.ac.cn>
2017-12-22 12:49:58 +0800

    stsytem_structure

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/system_structure', function (req, res, next) {
+  res.render('system_structure');
+});
 module.exports = router;
