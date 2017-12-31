3558823b39d93260f174b197a53bcc56e7692006
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-01 01:23:53 +0800

    增加留言板需要的中间件

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+
+var expressValidator = require('expressValidator');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
+var multer = require('multer');
+var flash = require('connect-flash')
 var index = require('./routes/index');
 var system_structure = require('./routes/system_structure');
 
@@ -18,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+app.use(multer({dest:'./public/image/uploads'}))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,8 +31,35 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret:'secret',
+  saveUninitialized:true,
+  resave:true
+}));
+
+app.use(expressValidator({
+  errorFormatter:function(param,msg,value){
+    var namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+    wihle(namespace.length){
+     fromParam += '[' + namespace.shift() + ']'; 
+    }
+    return {
+      param : formParam,
+      msg : msg,
+      value :value     
+    };
+  }
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(flash());
+app.use(function(req,res,next){
+  res.locals.message = require('express-messages')(req,res);
+  next();
+})
 app.use('/', index);
 app.use('/system_structure.html', system_structure);
 
