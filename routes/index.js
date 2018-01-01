79a346edce584bc59b5ab65f1a03921facaa87be
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-01 18:11:23 +0800

    增加留言板页面

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+router.get('/page_comment.html', function(req, res, next) {
+  res.render('page_comment');
+});
 
 module.exports = router;
