7563a6e52b7a7b1a2adb8a07078f5ceb0b45801e
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-02 00:21:21 +0800

    评论数据检索更改

--- routes/comment.js
+++ routes/comment.js
@@ -4,9 +4,9 @@ var pg = require('pgconn');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   sql="select * from posts order by index desc limit 5;";
-	my_conn.query(sql,function(result){		
+	pg.query(sql,function(result){		
 		res.jsonp(result.rows);
-		console.log(result.rows);
+		//console.log(result.rows);
 	}); 
   res.render('page_comment', { title: 'Comments' });
 });
