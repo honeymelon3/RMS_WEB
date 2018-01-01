3c523273a8fc45828a82a01c07a16e7e3a4c7fbe
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-02 00:55:58 +0800

    把数据传到前端

--- routes/comment.js
+++ routes/comment.js
@@ -2,15 +2,21 @@ var express = require('express');
 var router = express.Router();
 var pg = require('./pgconn');
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  sql="select * from posts order by index desc limit 5;";
-	pg.query(sql,function(result){		
-		console.log(result.rows);
-		//console.log(result.rows);
-	}); 
-  res.render('page_comment', { title: 'Comments' });
+
+    res.render('page_comment', { title: 'Comments' });
+
 });
 
-
-
+router.get('/posts', function(req, res, next) {
+  sql="select * from posts order by index desc limit 5;";
+	pg.query(sql,function(result){		
+		res.jsonp(result.rows);
+    //console.log(result.rows); 
+    
+	}); 
+ 
+});
 module.exports = router;
