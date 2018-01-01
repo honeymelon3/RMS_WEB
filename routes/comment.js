2020ef289f077988f8ddd54ab726072646dd6c2d
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-02 00:23:32 +0800

    修改comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pgconn');
+var pg = require('./pgconn');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   sql="select * from posts order by index desc limit 5;";
