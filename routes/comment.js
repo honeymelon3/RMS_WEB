5eb07ca4eaaf4e16fb7fade741797315c7aefe0f
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-02 14:39:50 +0800

    优化评论输入页面

--- routes/comment.js
+++ routes/comment.js
@@ -17,12 +17,33 @@ router.get('/posts', function(req, res, next) {
     //console.log(result.rows); 
     
   }); 
-  
-  router.get('/add', function (req, res, next) {
+  });
+router.get('/add', function (req, res, next) {
 
     res.render('add_comment', { title: 'Add comments' });
 
   });
- 
-});
+  router.post('/add', function (req, res, next) {
+        // 获得form Value
+      var  titile =req.body.title;
+      var  category = req.body.category;
+      var  body = req.body.body;
+      var  author = req.body.editor;
+      var  date   = new Date();
+      if (req.files.mainimage){
+         var mainImageOriginalName= req.files.mainimage.originalname;
+         var mainImageName = req.files.mainImage.name;
+         var mainImageMime = req.files.mainImage.mimetype;
+         var mainImagePath = req.files.mainImage.path;
+         var mainImageExt = req.files.mainImage.extension;
+         var mainImageSize = req.files.mainImage.size;
+      } else{
+        var mainImageName = "noimage.png";
+      } 
+
+      req.checkbody
+    res.render('add_comment', { title: 'Add comments' });
+
+  });
+
 module.exports = router;
