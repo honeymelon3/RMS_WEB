93d0ce21738cb60a2731a0a49465a8191da6dacf
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-25 00:44:41 +0800

    增加了kindEditor图片上传功能

--- routes/comment.js
+++ routes/comment.js
@@ -2,6 +2,7 @@ var express = require('express');
 var dateU = require('date-utils')
 var router = express.Router();
 var pg1 = require('./pgconn');
+var formidable=require('formidable');
 /* GET home page. */
 
 
@@ -74,5 +75,33 @@ router.post('/add', function (req, res, next) {
        
 
   });
+
+
+//上传图片
+app.post('/uploadImg',function(req,res,next){
+  var form=new formidable.IncomingForm();
+  form.keepExtensions=true;     //设置该属性为true可以使得上传的文件保持原来的文件的扩展名。
+  form.uploadDir=__dirname+'/public/upload';   //设置上传文件存放的文件夹，默认为系统的临时文件夹，可以使用fs.rename()来改变上传文件的存放位置和文件名
+  //form.parse(request, [callback]) 该方法会转换请求中所包含的表单数据，callback会包含所有字段域和文件信息
+  form.parse(req,function(err,fields,files){
+      if(err){
+          throw err;
+      }
+      var image=files.imgFile;  //这是整个files流文件对象,是转换成有利于传输的数据格式
+      var path=image.path;      //从本地上传的资源目录加文件名:如E:\\web\\blog\\upload\\upload_0a14.jpg
+      /*下面这里是通过分割字符串来得到文件名*/
+      var arr=path.split('\\');//注split可以用字符或字符串分割
+      var name=arr[arr.length-1];
+      /*上面这里是通过分割字符串来得到文件名*/
+      var url="/upload/"+name;
+      console.log(url);
+      var info = {
+          "error": 0,
+          "url": url
+      };
+      //info是用来返回页面的图片地址
+      res.send(info);
+  })
+})
 
 module.exports = router;
