39a7c725c44cff3287c12c1f186465640fd34b1a
Johnson Han <hanlifeng@sinap.ac.cn>
2018-01-29 23:31:20 +0800

    跨域访问问题尝试

--- app.js
+++ app.js
@@ -86,4 +86,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
+
 module.exports = app;
