279c4c9f3863ec9ffabc8f7f2223e43ad517c540
Johnson Han <hanlifeng@sinap.ac.cn>
2018-02-07 22:50:04 +0800

    add tank_widget demo

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Layout' });
 });
 
+router.get('/widget_tank', function(req, res, next) {
+  res.render('widget_tank', { title: 'Widget tank' });
+});
 
 
 
