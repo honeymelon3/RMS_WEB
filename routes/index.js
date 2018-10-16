072ea894b52311a5c92f6c9fba3af461831f7f9d
Johnson Han <hanlifeng@sinap.ac.cn>
2018-10-17 01:09:18 +0800

    debug 登录

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.route("/login").get(function(req,res){    // 到达此路径则渲染logi
 			res.sendStatus(404);							//	状态码返回404
 			//	res.redirect("/login");
 		}else if(result.rowCount == 1){
-			////console.log(result.rows[0].username);
+			console.log(result.rows[0]);
 			req.session.user = result.rows[0]; 
 			console.log(result.rows[0]);
 			res.sendStatus(200);
@@ -56,10 +56,11 @@ router.get('/task_data', function (req, res, next) {
 
 
 router.get('/task', function(req, res, next) {
-	if(req.session.user.corrode == 1){ 					//到达/home路径首先判断是否已经登录
+	console.log(req.session.user.corrode);
+	if(req.session.user == 1){ 					//到达/home路径首先判断是否已经登录
 		res.render('task', { title: '任务清单'});   			//未登录则重定向到 /login 路径
 	} ;	
-	if(req.session.user.corrode == 0){ 					//到达/home路径首先判断是否已经登录
+	if(req.session.user == 0){ 					//到达/home路径首先判断是否已经登录
 		res.render("/login"); 			//未登录则重定向到 /login 路径
 	} ;	
   
